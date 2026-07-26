a8cd8e81a7f87656cd13c7ce6bdcb1ec9ccbeee0
msohamart-628 <msoham628@gmail.com>
2026-07-26 15:23:56 +0530

    day 01

---
+++ index.js
@@ -0,0 +1,56 @@
+console.log("Hello, World!");
+console.log(32)
+console.log("The value of 32 is:"+ 32)
+console.log(5>3)
+console.log(5+3)
+console.log("The sum of 5 and 3 is: "+ (5+3))
+console.log(`Hare Krishna
+    This is Gurukulam STW`)
+console.log('5'+'3')
+console.log('5'-'3')
+console.log('5'===5)//strictly equality operatorc
+/*
+value of 5 is: 5
+*/
+console.log(`This is a text, this is the value of 5+3: ${5+3}`)//strictly equality operatorc
+
+const name = "Gurukulam STW"
+console.log(`Hey ${name}, How are you?`)
+
+let a = 5
+let b = 3
+var c = 10
+console.log(a+b)
+
+a = 10
+console.log(a+b)
+console.log(typeof a)
+console.log(typeof name)
+let typeOfA = typeof a
+console.log(typeOfA)
+console.log(c+5)
+
+a = "Hello"
+//name = 32
+const array = [1,2,3,4,5]
+console.log(array)
+//const input = prompt("Enter your name:")
+//console.log("Your name is: ", input)
+console.log(typeof array)
+const obj ={
+    name:{
+        firstName: "Soham",
+        lastName: "Mondal"
+    },
+    location: "India",
+    isStudent: true
+}
+console.log(typeof obj == typeof array)
+
+const abc = null
+const def = undefined
+console.log(abc===def)
+console.log(typeof def)
+console.log(obj.name.firstName)
+console.log(obj.name.lastName)
+console.log(obj.school.name)
